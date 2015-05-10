6beba4c8ac9dc896a072117439bf4897cf223a9e
nimbinatus <nimbinatus@users.noreply.github.com>
2015-05-09 21:49:20 -0500

    Working on the game from PyCharm for the first time.

--- ex43.py
+++ ex43.py
@@ -1,8 +1,9 @@
 from sys import exit
 from random import randint
 
+
 class Scene(object):
-    
+
     def enter(self):
         print "Scene not made. Exiting."
         exit(1)
@@ -19,31 +20,38 @@ class Engine(object):
 
         while current_scene != final_scene:
             print "While-loop ran!"
-            next_scene_ID = current_scene.enter()
-            current_scene = self.scene_map.next_scene(next_scene_ID)
+            next_scene_id = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_id)
 
-        #TODO: No idea why this one isn't working at the end.
         current_scene.enter()
         
         
 class Death(Scene):
 
+    deaths = {"1": "You died. Game over.",
+              "2": "You died 2. Game over.",
+              "3": "You died 3. Game over.",
+              "4": "You died 4. Game over.",
+              "5": "5. Game over.",
+              "6": "6. Game over.",
+              "7": "7. Game over.",
+              "8": "8. Game over.",
+              "9": "9. Game over.",
+              "10": "Infinity death. Game over."
+              }
+
     def enter(self):
-        print "You died. Game over."
+        print Death.deaths[str(randint(1, 10))]
         exit(1)
 
 
 class CentralCorridor(Scene):
 
     def enter(self):
-        print ("You are the last person left alive after the Gothons from"
-               " Planet Percal #25 have overtaken your ship. You have been on"
-               " the run since your captain sent out a shipwide call early in"
-               " your shift. To prevent your ship from falling into Gothon "
-               "control, you need to get to the armory to get a neutron bomb to"
-               " set off. This is the starting point and has a Gothon already "
-               "standing there they have to defeat with a joke before continuing."
-        )
+        # TODO: Write up starting story.
+        print ("This is the starting point and has a Gothon already standing"
+               " there they have to defeat with a joke before continuing."
+               )
         answer = raw_input('> ')
         if answer == 'yes':
             return 'armory'
@@ -54,10 +62,11 @@ class CentralCorridor(Scene):
 class LaserWeaponArmory(Scene):
 
     def enter(self):
+        # TODO: Write up story.
         print ("This is where the hero gets a neutron bomb to blow up the ship"
                " before getting to the escape pod. It has a keypad the hero"
                " has to guess the number for."
-        )
+               )
         answer = raw_input('> ')
         if answer == 'yes':
             return 'bridge'
@@ -68,9 +77,10 @@ class LaserWeaponArmory(Scene):
 class TheBridge(Scene):
 
     def enter(self):
+        # TODO: Write up story.
         print ("Another battle scene with a Gothon where the hero places the"
                " bomb."
-        )
+               )
         answer = raw_input('> ')
         if answer == 'yes':
             return 'escape'
@@ -81,26 +91,34 @@ class TheBridge(Scene):
 class EscapePod(Scene):
 
     def enter(self):
+        # TODO: Write up story.
         print ("Where the hero escapes but only after guessing the right"
                " escape pod."
-        )
+               )
         answer = raw_input('> ')
         if answer == 'yes':
             return 'end'
         else:
             return 'death'
 
+
+class Ending(Scene):
+
+    def enter(self):
+        # TODO: Write up story.
+        print "Winning scenario."
+        exit()
+
+
 class Map(object):
 
-
-    setmap = { 'central_corridor' : CentralCorridor(),
-               'armory' : LaserWeaponArmory(),
-               'bridge' : TheBridge(),
-               'escape' : EscapePod(),
-               'death' : Death(),
-               'end' : 'You win!'
-    }
-
+    setmap = {'central_corridor': CentralCorridor(),
+              'armory': LaserWeaponArmory(),
+              'bridge': TheBridge(),
+              'escape': EscapePod(),
+              'death': Death(),
+              'end': Ending()
+              }
 
     def __init__(self, start_scene):
         self.start_scene = start_scene
